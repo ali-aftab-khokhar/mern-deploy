e69dcd0a4a091819585f2386680963eda7ea5f67
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-09-23 15:21:28 +0500

    login api edit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {login, register} = require('../controllers/userController')
 
-router.post('/api', login)
+router.post('/api/', login)
 
 router.post('/api/register', register);
 
